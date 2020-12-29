06a8af45fae3f59546f38ff8ce8a58a14970ada9
leijf <leijinfeng@geetest.com>
2020-12-29 17:12:22 +0800

    [StringGuard][feature/0.2.0][update][1.增加上传 jcenter 脚本 2.加密算法实现类改为一个 module 多个 package 模式]

--- app/src/main/java/com/geetest/stringguard/MainActivity.java
+++ app/src/main/java/com/geetest/stringguard/MainActivity.java
@@ -1,9 +1,10 @@
 package com.geetest.stringguard;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     // Test static final fields
